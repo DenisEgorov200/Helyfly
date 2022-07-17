3d425ec0a34931e4508c7f4989b18f319ef70361
DenisEgorov20 <egorov.denis.mem@gmail.com>
2022-07-17 19:06:01 +0300

    responsive layout

---
+++ src/assets/js/photo-slider.js
@@ -0,0 +1,10 @@
+new Swiper('.photo__slider', {
+
+    slidesPerView: 1,
+
+    centeredSlides: true,
+
+    loop: true,
+
+    spaceBetween: 30,
+});
--- src/assets/js/show-more.js
+++ src/assets/js/show-more.js
@@ -1,34 +1,34 @@
+// // const showMore = document.querySelector('.photo__more');
+// // const photoLength = document.querySelectorAll('.gallery__item').length;
+// // let items = 8;
+
+// // showMore.addEventListener('click', function() {
+// //     items += 4;
+// //     const array = Array.from(document.querySelector('.gallery').children);
+// //     const visItems = array.slice(0, items);
+
+// //     visItems.forEach(el => el.classList.add('is-visible'));
+
+// //     if (visItems.length === photoLength) {
+// //         visItems.forEach(el => el.classList.remove('is-visible'));
+// //     }
+// // });
+
 // const showMore = document.querySelector('.photo__more');
-// const photoLength = document.querySelectorAll('.gallery__item').length;
+// const photoLength = document.querySelectorAll('.gallery__item');
+// const arrowImg = document.querySelectorAll('.photo__img')
+// console.log(arrowImg);
 // let items = 8;
 
 // showMore.addEventListener('click', function() {
+//     for (let i = items; i < items + 4; i++) {
+//         if (photoLength[i]) {
+//             photoLength[i].style.display = 'block';
+//         }
+//     }
 //     items += 4;
-//     const array = Array.from(document.querySelector('.gallery').children);
-//     const visItems = array.slice(0, items);
-
-//     visItems.forEach(el => el.classList.add('is-visible'));
-
-//     if (visItems.length === photoLength) {
-//         visItems.forEach(el => el.classList.remove('is-visible'));
+//     if (items === photoLength.length) {
+//         // event.target.style.display = 'none'
+//         showMore.style.display = 'none'
 //     }
 // });
-
-const showMore = document.querySelector('.photo__more');
-const photoLength = document.querySelectorAll('.gallery__item');
-const arrowImg = document.querySelectorAll('.photo__img')
-console.log(arrowImg);
-let items = 8;
-
-showMore.addEventListener('click', function() {
-    for (let i = items; i < items + 4; i++) {
-        if (photoLength[i]) {
-            photoLength[i].style.display = 'block';
-        }
-    }
-    items += 4;
-    if (items === photoLength.length) {
-        // event.target.style.display = 'none'
-        showMore.style.display = 'none'
-    }
-});
